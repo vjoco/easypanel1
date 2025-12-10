8b34cd4e1f967793803ab559458f13e55d0ce867
Raspi <victor.joco@inl.int>
2025-12-10 12:01:54 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// Set the content type header
+header('Content-Type: text/html; charset=utf-8');
+
+// Display the Hello World message
+echo "<h1>Hello World from Easypanel!</h1>";
+echo "<p>This is a simple PHP app running in a container.</p>";
+?>
